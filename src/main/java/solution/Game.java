60c8f075f41ef1a6a1976bf7d44e4efd491e4f76
mohamed <mohamed.bouhamed@lcdlv.com>
2017-09-27 12:23:45 +0200

    make it works

---
+++ src/main/java/solution/Game.java
@@ -0,0 +1,95 @@
+package solution;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Game {
+    public static final String END = "E";
+    public static final String START = "S";
+    public static final String DICE = "R";
+    public static final int INIT_VALUE = -1;
+    private int startCase;
+    private int endCase;
+    private List<String> gameCases;
+    static Map<Integer, Integer> visitedCase;
+    public static AtomicInteger minGame;
+
+    public Game(List<String> gameSteps) {
+        this.gameCases = gameSteps;
+        startCase = findPosition (START);
+        endCase = findPosition (END);
+        visitedCase = new HashMap<Integer, Integer> ( );
+        minGame = new AtomicInteger (-1);
+
+    }
+
+    public int getStartCase() {
+        return startCase;
+    }
+
+    public List<String> getGameCases() {
+        return Collections.unmodifiableList (gameCases);
+    }
+
+    public int updatePosition(int caseElement) {
+        final int newPosition = caseElement + Integer.parseInt (getGameCases ( ).get (caseElement));
+        if ( notInRangeGame (newPosition) )
+            return getStartCase ( );
+        return newPosition;
+    }
+
+    public boolean isDiceCaseElement(int element) {
+        final String gameCase = getGameCases ( ).get (element);
+        return gameCase.equals (DICE) || gameCase.equals (START);
+    }
+
+    public int updateAndGetMinGame(int game) {
+        if ( game < minGame.get ( ) || minGame.get ( ) == INIT_VALUE )
+            minGame.set (game);
+        return minGame.get ( );
+    }
+
+    public boolean endGame(int caseElement) {
+        return caseElement == endCase;
+    }
+
+    public boolean isGameOver(int caseElement, int gameNumber) {
+        return overMinGame (gameNumber) ||
+                notInRangeGame (caseElement) ||
+                isAlreadyVisitTheCase (caseElement, gameNumber);
+
+    }
+
+    private boolean notInRangeGame(int caseElement) {
+        return !(caseElement >= 0 && caseElement < getGameCases ( ).size ( ));
+    }
+
+    private int findPosition(String caseElementToFind) {
+        int position = 0;
+        for (String gameStep : getGameCases ( )) {
+            if ( gameStep.equals (caseElementToFind) )
+                return position;
+            position++;
+        }
+        throw new IllegalArgumentException ( );
+    }
+
+    private boolean isAlreadyVisitTheCase(int caseElement, int gameNumber) {
+        Integer value = visitedCase.get (caseElement);
+        if ( value == null || ((value != null) && (value > gameNumber)) ) {
+            visitedCase.put (caseElement, gameNumber);
+            return false;
+        }
+        return true;
+
+    }
+
+    private boolean overMinGame(int game) {
+        final int minGameValue = minGame.get ( );
+        return game > minGameValue && minGameValue != INIT_VALUE;
+    }
+
+}
--- src/main/java/solution/Solution.java
+++ src/main/java/solution/Solution.java
@@ -2,12 +2,40 @@ package solution;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
 
 
     public static int compute(BufferedReader br) throws IOException {
-        return 3;
+        final List<String> gameSteps = br.lines ( ).collect (Collectors.toList ( ));
+        Game caseGame = new Game (gameSteps);
+        return seek (caseGame, caseGame.getStartCase ( ), 0);
+    }
+
+
+    public static int seek(Game gameCase, int start_Case,
+                           int game) {
+        if ( gameCase.endGame (start_Case) )
+            return gameCase.updateAndGetMinGame (game);
+        if ( gameCase.isGameOver (start_Case, game) ) {
+            return gameCase.updateAndGetMinGame (Game.minGame.get ( ));
+        } else {
+
+            if ( gameCase.isDiceCaseElement (start_Case) ) {
+                seek (gameCase, start_Case + 6, game + 1);
+                seek (gameCase, start_Case + 5, game + 1);
+                seek (gameCase, start_Case + 4, game + 1);
+                seek (gameCase, start_Case + 3, game + 1);
+                seek (gameCase, start_Case + 2, game + 1);
+                seek (gameCase, start_Case + 1, game + 1);
+            } else
+                seek (gameCase, gameCase.updatePosition (start_Case), game + 1);
+
+            return gameCase.updateAndGetMinGame (Game.minGame.get ( ));
+        }
+
     }
 
 
